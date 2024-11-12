b089e68478da4b13a2385e990abdd2308364dcb9
AlmidonTapia <221192@unamba.edu.pe>
2024-11-12 14:45:58 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PersonInsertComponent } from './pages/person/insert/person-insert.component';
+import { PersonGetAllComponent } from './pages/person/getall/person-get-all.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: '/', pathMatch: 'full' },
+	{ path: 'person/insert',
+	component: PersonInsertComponent },
+	{ path: 'person/getall',
+	component: PersonGetAllComponent }
+];
