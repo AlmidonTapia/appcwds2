6af269a41d22e1c948b0cd8ac598635e0b1d6a12
AlmidonTapia <221192@unamba.edu.pe>
2024-12-31 15:19:45 -0500

    refactor: actualizar URLs de servicio en person.service.ts para el backend .NET

--- src/app/api/person.service.ts
+++ src/app/api/person.service.ts
@@ -9,25 +9,36 @@ import { Observable, retry } from 'rxjs';
 export class PersonService {
 	constructor(
 		private httpClient: HttpClient
-	) {}
+	) { }
 
 	public getData(): Observable<any> {
 		return this.httpClient.get(`http://localhost:8080/person/getdata`);
+		// backsito de .net
+		// return this.httpClient.get(`http://localhost:5191/person/getdata`);
 	}
 
 	public insert(formData: FormData): Observable<any> {
 		return this.httpClient.post(`http://localhost:8080/person/insert`, formData);
+		//backsito de .net
+		// return this.httpClient.post(`http://localhost:5191/person/insert`, formData);
+
 	}
 
 	public getAll(): Observable<any> {
 		return this.httpClient.get(`http://localhost:8080/person/getall`);
+		// backsito de .net
+		// return this.httpClient.get(`http://localhost:5191/person/getall`);
 	}
 
 	public delete(idPerson: string): Observable<any> {
 		return this.httpClient.delete(`http://localhost:8080/person/delete/${idPerson}`);
+		// backsito de .net
+		// return this.httpClient.delete(`http://localhost:5191/person/delete/${idPerson}`);
 	}
 
 	public update(formData: FormData): Observable<any> {
 		return this.httpClient.post(`http://localhost:8080/person/update`, formData);
+		// backsito de .net
+		// return this.httpClient.post(`http://localhost:5191/person/update`, formData);
 	}
 }
